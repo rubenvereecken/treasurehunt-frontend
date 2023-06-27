d99af55ed439b97142a3275bdcaf688dbee1d07a
Ruben Vereecken <rubenvereecken@gmail.com>
2023-06-27 13:35:18 +0100

    test

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -88,7 +88,8 @@ export function useMessages({ roomSlug }: { roomSlug: string }): {
   if (!data) return { loading, error, data: [] };
 
   const messages: IMessage[] = (data.messageCollection.edges ?? [])
-    .map((edge) => JSON.parse(JSON.stringify(edge.node)))
+    // .map((edge) => JSON.parse(JSON.stringify(edge.node)))
+    .map((edge) => edge.node)
     .filter((msg) => msg.room.slug == roomSlug);
 
   // mVessages.map((msg) => {
